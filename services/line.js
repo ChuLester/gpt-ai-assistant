59025a0feeec070088148abfdd74b6ce4408ebfd
ChuLester <dxxlin2065@gmail.com>
2023-03-01 20:59:43 +0800

    add replay condition

--- services/line.js
+++ services/line.js
@@ -31,13 +31,19 @@ instance.interceptors.request.use((c) => {
   return c;
 });
 
-const reply = ({
-  replyToken,
-  messages,
-}) => instance.post('/v2/bot/message/reply', {
-  replyToken,
-  messages,
-});
+const reply = ({ replyToken, messages }) => {
+  const textMessage = messages.find(
+    (message) =>
+      message.type === 'text' && message.text.includes('Q:')
+  );
+  if (!textMessage) {
+    // 如果沒有包含 "Q:" 的文字訊息，就不做任何事情，直接返回
+    return;
+  }
+
+  // 如果有包含 "Q:" 的文字訊息，就繼續執行後面的程式碼
+  return instance.post('/v2/bot/message/reply', { replyToken, messages });
+};
 
 const fetchProfile = ({
   userId,
